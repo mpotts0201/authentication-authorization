bc55f3939020114c430880457e95a7c5acb78d1e
king0120 <king0120@gmail.com>
2018-04-06 11:28:58 -0400

    added signuplogin

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import SignUpLogIn from './components/SignUpLogIn'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <SignUpLogIn />
+      </div>
+    )
+  }
+}
+
+export default App
