0291f5133cc7608002e2d14c6a4c9a352c200178
mpotts0201 <mpotts0201@gmail.com>
2018-04-06 16:02:55 -0400

    updating after merge and pull

--- client/src/components/SignUpLogIn.js
+++ client/src/components/SignUpLogIn.js
@@ -1,71 +1,9 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-=======
 
 import React, { Component } from 'react'
->>>>>>> d5a0695f7600baf6827878aeec2808dcd3885b1c
 
 class SignUpLogIn extends Component {
 
     state = {
-<<<<<<< HEAD
-        email: '',
-        password: '',
-        password_confirmation: ''
-    }
-
-    signUp = (event) => {
-        event.preventDefault()
-        this.props.signUp(
-            this.state.email,
-            this.state.password,
-            this.state.password_confirmation
-        )
-    }
-
-    signIn = (event) => {
-        event.preventDefault()
-        this.props.signIn(
-            this.state.email,
-            this.state.password
-        )
-    }
-
-    handleChange = (event) => {
-        const newState = {...this.state}
-        newState[event.target.name] = event.target.value
-        this.setState(newState)
-    }
-
-
-    render() {
-        return (
-            <div>
-                <form>
-                    <div>
-                        <label htmlFor="email">E-mail: </label>
-                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email}/>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password: </label>
-                        <input onChange={this.handleChange} type="password" name="password" value={this.state.password}/>
-                    </div>
-                    <div>
-                        <label htmlFor="password_confirmation">Confirm Password: </label>
-                        <input onChange={this.handleChange} type="password" name="password_confirmation"
-                               value={this.state.password_confirmation}/>
-                    </div>
-
-                    <button onClick={this.signUp}>Sign Up</button>
-                    <button onClick={this.signIn}>Log In</button>
-                </form>
-            </div>
-        );
-    }
-}
-
-export default SignUpLogIn;
-=======
       email: '',
       password: '',
       password_confirmation: ''
@@ -121,4 +59,3 @@ export default SignUpLogIn;
 }
 
 export default SignUpLogIn
->>>>>>> d5a0695f7600baf6827878aeec2808dcd3885b1c
