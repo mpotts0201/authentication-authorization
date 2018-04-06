105f3864e349dc069d6f257bb8ef3183e4675956
king0120 <king0120@gmail.com>
2018-04-06 11:53:03 -0400

    updated app js with signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import axios from 'axios'
 import SignUpLogIn from './components/SignUpLogIn'
 
 class App extends Component {
+  state = {
+    signedIn: false
+  }
+
+  signUp = async (email, password, password_confirmation) => {
+    try {
+      const payload = {
+        email,
+        password,
+        password_confirmation
+      }
+      const response = await axios.post('/auth', payload)
+      console.log(response.data)
+
+      this.setState({ signedIn: true })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  signIn = async (email, password) => {
+    try {
+      const payload = {
+        email,
+        password
+      }
+      const response = await axios.post('/auth/sign_in', payload)
+      console.log(response.data)
+
+      this.setState({ signedIn: true })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   render () {
+    const SignUpLogInComponent = () => (
+      <SignUpLogIn
+        signUp={this.signUp}
+        logIn={this.logIn}
+      />
+    )
+
     return (
-      <div className="App">
-        <SignUpLogIn />
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/signUp" render={SignUpLogInComponent}/>
+          </Switch>
+          {this.state.signedIn ? null : <Redirect to="signUp"/>}
+        </div>
+      </Router>
     )
   }
 }
